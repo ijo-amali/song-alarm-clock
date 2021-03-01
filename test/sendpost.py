2fd00428e6bbda1ebadebf53dfe7cc31c8a9fbde
levistep32 <levistep32@gmail.com>
2021-02-28 23:03:47 -0700

    more debug stuff

--- test/sendpost.py
+++ test/sendpost.py
@@ -1,11 +1,14 @@
 import requests
 
+print('Testing~\n\n')
+
 URL = 'http://10.0.0.185:8080'
 
 r = requests.get(URL)
 
-print(r.text + "\n")
+print("Get request: %s\n" % r.text)
 
 r = requests.post(URL, data={"url": "kzVVVNT4ic4"})
 
-print(r.text)  # displays the result body.
+print("Post request: %s\n" % r.text)
+print(r)
