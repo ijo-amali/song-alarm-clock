2e5c6a5db07955b4c6f01ecb46923d6bec7f379d
levistep32 <levistep32@gmail.com>
2021-03-01 01:13:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources="http://song-clock.glitch.me")
+CORS()
 
 
 @app.route('/', methods=['GET'])
